cc97059854365f09b134b76ade35f5a4b6a3fec1
Jared Meier <jared.meier@rewardstyle.com>
2024-07-15 18:08:17 -0500

    Test for thank you message

--- TAUUITests/TAUUITests.swift
+++ TAUUITests/TAUUITests.swift
@@ -41,6 +41,16 @@ final class TAUUITests: XCTestCase {
 
     }
 
+    func testThankYouMessage() {
+        let app = XCUIApplication()
+        app.launch()
+        app.textFields["city"].tap()
+        app.typeText("Houston")
+        app.buttons["enrollButton"].tap()
+        XCTAssertTrue(app.staticTexts["Thanks for Joining!"].exists)
+
+    }
+
     func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
             // This measures how long it takes to launch your application.
