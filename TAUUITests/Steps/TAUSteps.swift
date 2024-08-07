b2d7d85634b439e56f642d61b58398323825c707
Jared Meier <jared.meier@rewardstyle.com>
2024-08-07 16:34:02 -0500

    Add Base, Steps, XCUIElements groups. Also add an ennum for the TAUScreen elements

--- TAUUITests/Steps/TAUSteps.swift
+++ TAUUITests/Steps/TAUSteps.swift
@@ -1,46 +1,35 @@
 //
-//  BDDTest.swift
+//  TAUSteps.swift
 //  TAUUITests
 //
-//  Created by Jared Meier on 7/16/24.
+//  Created by Jared Meier on 8/7/24.
 //  Copyright © 2024 Shashikant Jagtap. All rights reserved.
 //
 
 import Foundation
 import XCTest
 
-class BDDTest: TAUUITestBase {
-
-    func testThankYouMessageInBDDStyle() {
-
-        givenAppIsReady()
-        whenIEnterCity(city: "Houston")
-        whenIEnrolled()
-        thenIShouldSeeThankYouMessage()
-    }
-}
-
 extension TAUUITestBase {
 
     func givenAppIsReady() {
 
         XCTContext.runActivity(named: "Given app is ready ") { _ in
-            XCTAssertTrue(app.buttons["enrollButton"].exists)
+            XCTAssertTrue(TAUScreen.enrollButton.element.exists)
         }
     }
 
     func whenIEnterCity(city: String) {
 
         XCTContext.runActivity(named: "When I enter a city \(city) ") { _ in
-            app.textFields["city"].tap()
-            app.textFields["city"].typeText(city)
+            TAUScreen.cityTextField.element.tap()
+            TAUScreen.cityTextField.element.typeText(city)
         }
     }
 
     func whenIEnrolled() {
 
         XCTContext.runActivity(named: "When I Enrolled " ) { _ in
-            app.buttons["enrollButton"].tap()
+            TAUScreen.enrollButton.element.tap()
         }
     }
 
